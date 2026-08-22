2683672714af68bfa05a6a6a6d55e8b2ac5ee5cb
Ivan <igp183@outlook.com>
2026-08-22 20:16:24 +0100

    changes: UI, color scheme, new page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
+// Carregamos a fonte Inter
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,10 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt" className="dark scroll-smooth">
-      <body className={`${inter.className} flex flex-col min-h-screen`}>
+    <html lang="pt" className="scroll-smooth">
+      {/* O inter.className garante que o Inter é usado em todo o site */}
+      <body className={`${inter.className} flex flex-col min-h-screen bg-slate-50 text-slate-800`}>
         <Navbar />
-        {/* Main content area expands to push footer to the bottom */}
         <div className="flex-grow">
           {children}
         </div>
